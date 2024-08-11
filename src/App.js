2f85ae479c1f0ad2d4db174cbe8137a2b7cc1bff
Sero <kmcaltiz@gmail.com>
2024-08-11 21:14:27 +0800

    Create app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import Missing from "./components/Missing";
+import About from "./components/About";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const App = () => {
   return (
-    <div className='App'>
-
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <Route path="/" Component={App} />
+      <Routes>
+        <Route path="/" Component={App} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
