7885a475fb223fcf72573c2112edafe6d6d79511
Sero <kmcaltiz@gmail.com>
2024-08-12 11:18:27 +0800

    add link for missing

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Missing = () => {
   return (
     <div className="missing">
-      <h1>Missing page</h1>
+      <h2>Page Not Found</h2>
+      <p>Well, that's disappointing</p>
+      <p>
+        <Link to={"/"}>Visit Our Homepage</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Missing
+export default Missing;
