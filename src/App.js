9bbda7c28b215f587b20cf07021ee2435a71b393
Sero <kmcaltiz@gmail.com>
2024-08-12 10:57:54 +0800

    install date-fns, make nav responsive, styles new post form, complete handledelete and handle submit, create new state for post title and post body

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import NewPost from "./components/NewPost";
 import PostPage from "./components/PostPage";
 import Missing from "./components/Missing";
 import About from "./components/About";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { format } from "date-fns";
 
 const App = () => {
   const [posts, setPosts] = useState([
@@ -38,10 +39,32 @@ const App = () => {
   ]);
   const [seach, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-
+  const [postTitle, setPostitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const navigate = useNavigate();
   const handleDelete = (id) => {
+    const postList = posts.filter((post) => post.id !== id);
+    setPosts(postList);
+    navigate("/");
+  };
 
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      datetime,
+      body: postBody,
+    };
+    const allPosts = { ...posts, newPost };
+    setPosts(allPosts);
+    setPostitle("");
+    setPostBody("");
+    navigate("/");
+  };
+
   return (
     <div className="App">
       <Header title={"React Blog App"} />
@@ -49,7 +72,19 @@ const App = () => {
       <main className="main">
         <Routes>
           <Route exact path="/" element={<Home posts={posts} />} />
-          <Route exact path="/post" element={<NewPost />} />
+          <Route
+            exact
+            path="/post"
+            element={
+              <NewPost
+                postTitle={postTitle}
+                setPostTitle={setPostitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+                handleSubmit={handleSubmit}
+              />
+            }
+          />
           <Route
             path="/post/:id"
             element={<PostPage posts={posts} handleDelete={handleDelete} />}
--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-const NewPost = () => {
+const NewPost = ({
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+  handleSubmit,
+}) => {
   return (
     <div className="new-post">
-      <h1>New Post</h1>
+      <h2>New Post</h2>
+      <form className="new-post-form" onSubmit={handleSubmit}>
+        <label htmlFor="post-title">Title:</label>
+        <input
+          type="text"
+          id="post-title"
+          required
+          value={postTitle}
+          autoComplete="off"
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="post-body">Post:</label>
+        <textarea
+          name="post-body"
+          autoComplete="off"
+          id="post-body"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        ></textarea>
+        <button type="submit" className="button-create-post">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
